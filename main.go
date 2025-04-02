4f19d8a1387ced03fee6e4adaa37dff16f3a8fe4
OminousShadow87 <brucewaynephil@gmail.com>
2025-04-02 16:19:56 -0700

    added health handler

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", healthHandler)
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	server.ListenAndServe()
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
